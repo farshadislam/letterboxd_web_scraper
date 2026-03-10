21caf473b5f70d0cb79c7d4d8daabd738f179a6d
farshadislam <fmislam.3105@gmail.com>
2026-03-10 02:15:59 -0600

    It gets the favourite film titles along with their respective release years!

--- scraper.js
+++ scraper.js
@@ -11,13 +11,13 @@ async function scrape() {
     const $ = cheerio.load(html); // Loads html into cheerio
     const favourites = []; // Gonna store user's favs in this list
 
-    $('div.favourite-production-poster-container').each((_i, el) => { // Should IDEALLY parse all favourites
-        /* don't need to go down every subclass!   div.favourite-production-poster-container */
+    $('div.favourite-production-poster-container div.react-component').each((_i, el) => { // Should IDEALLY parse all favourites
+        /* don't need to go down every subclass!*/
         
         
         
         /* REALLY INTERESTING because I had the right idea, but apparently the info is actually rendered on the client side or something? Either way, it looks like I need to read the meta tags instead in order to get the film titles */
-        const film_name_and_year = $(el).find('.frame-title').text().trim();
+        const film_name_and_year = $(el).attr('data-item-name');
 
         // Still need to find a way to get the highest res movie poster for each film
 
