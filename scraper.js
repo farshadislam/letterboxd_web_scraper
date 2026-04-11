18679704e1db64330d519076bc1b664e02447d7b
farshadislam <fmislam.3105@gmail.com>
2026-04-11 00:30:12 -0600

    nproc shows number of cores and also I outlined what needs to happen in scrapeReviews

--- scraper.js
+++ scraper.js
@@ -2,10 +2,7 @@ import { chromium } from 'playwright';
 
 async function scrapeFavourites(username) {
   const browser = await chromium.launch();
-  const context = await browser.newContext(); // This is required for multiple pages
-  
-  const page = await context.newPage();
-  const allPages = context.pages(); // Only one page for my Letterboxd profile on its own
+  const page = await browser.newPage();
 
   page.setDefaultTimeout(10_000);
   page.setDefaultNavigationTimeout(30_000);
@@ -14,9 +11,17 @@ async function scrapeFavourites(username) {
   await page.waitForSelector('.favourite-production-poster-container .frame-title');
  // This is where we get the film titles in my favourites bar
 
-  const titles = await page.locator('.favourite-production-poster-container .frame-title').evaluateAll(
-    els => els.map(el => el.textContent)
-  );
+  const titles = await page
+    .locator('.favourite-production-poster-container .frame-title') // Identifies tags
+    .evaluateAll( // “Run this function inside the browser (page) context, and give me back the result.”
+      els => els.map(el => el.textContent) // IMPLICITLY returns the els list
+
+      /*
+      els => {
+        return els.map(el => el.textContent) // EXPLICITLY returns the els list
+      }
+      */
+    );
 
   const nerdTitles = await page.$$eval('.favourite-production-poster-container .frame-title', els =>
     els.map(el => el.closest('a').getAttribute('href').split('/')[3]) // Want to get the third element from the split (contains film title)
@@ -24,29 +29,53 @@ async function scrapeFavourites(username) {
 
   const posterLinks = []; // Initialize outside of for-loop
 
-  for (let i = 0; i < nerdTitles.length; i++) {
+  /*
+  for (let i = 0; i < nerdTitles.length; i++) { 
     await page.goto(`https://letterboxd.com/film/${nerdTitles[i]}/`, { waitUntil: 'domcontentloaded' });
+    await page.waitForSelector('.poster film-poster .image'); // <--- Times out the entire thing goddammit
 
-    await page.evaluate(() => { // Runs a function inside of the headless browser
-      document.querySelector('#poster-modal').style.display = 'block'; // Sets the HD poster visible, so that we can inspect element the shit out of it (in theory)
-    });
-
-    posterLinks[i] = await page.$eval('#poster-modal img.image', el => el.getAttribute('src'));
+    posterLinks[i] = await page.$eval('#poster-modal img.image', el => el.getAttribute('srcset'));
   }
 
   await browser.close();
-  return { titles, posterLinks };
+  */
+  return { titles };
+}
+
+async function scrapeReviews(username) {
+  const browser = await chromium.launch();
+  const context = await browser.newContext(); // This is required for multiple pages
+  const allPages = context.pages(); // Should be two pages for how many reviews I've written
+
+  const firstPage = await context.newPage();
+  await firstPage.goto(`https://letterboxd.com/${username}/reviews/`, { waitUntil: 'domcontentloaded' });
+  
+  // When a DOM element has spaces in the class name, that means it's been assigned multiple classes
+
+  const reviews = await page.locator('.js-listitem .js-review .collapsed-text').evaluateAll(
+    els => {
+      const stupid = []
+
+      // Implement the logic to unpack every instance of more and then store every review on the page
+
+      // Fidn every more tag
+      // click on it or expand some fucking how
+      // unpack every p tag
+      // concatenate into a full review
+      // push to stupid list
+
+      return stupid;
+    }
+  );
 }
 
 async function main() {
-  const { titles, posterLinks } = await scrapeFavourites('orangepickleguy');
-
-  
-
+  const { titles } = await scrapeFavourites('orangepickleguy');
   console.log(titles);
-  console.log(posterLinks);
 
   // Want to add logic to sift through all the reviews next
+
+  
 }
 
 main();
